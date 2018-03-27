91913d3a73013cefa6e22ddab04616ef42c63e67
turboSlowPoke <mega_pokemon@mail.ru>
2018-03-27 17:55:09 +0300

    1

--- src/main/java/project01/project01/entyties/User.java
+++ src/main/java/project01/project01/entyties/User.java
@@ -1,5 +1,46 @@
 package project01.project01.entyties;
 
+import javax.persistence.*;
 
+@Entity
 public class User {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String login;
+    private String password;
+    @OneToOne(cascade = CascadeType.ALL)
+    private UserData userData;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
